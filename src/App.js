9ac4cb878a6093af1e01efad1f3f82b43ee904a2
sababilal <sabarangrez77@gmail.com>
2021-04-27 10:51:58 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,149 +1,162 @@
-import React, { useState, useEffect, useRef } from "react";
-import Axios from "axios";
-import { useHistory } from "react-router-dom";
-import Cookies from "js-cookie";
+  import React, { useState, useEffect, useRef } from "react";
+  import Axios from "axios";
+  import { useHistory } from "react-router-dom";
+  import Cookies from "js-cookie";
 
-function App() {
-  const backButton = useRef(null);
-  let history = useHistory();
-  var [qIndex, setQindex] = useState();
-  const [qtext, setQtext] = useState();
-  const [option, setOptions] = useState([]);
-  const [choosenOption, chooseOption] = useState("");
+  function App() {
+    const backButton = useRef(null);
+    let history = useHistory();
+    var [qIndex, setQindex] = useState();
+    const [qtext, setQtext] = useState();
+    const [option, setOptions] = useState([]);
+    const [choosenOption, chooseOption] = useState("");
 
-  useEffect(() => {
-  let userid=Cookies.get("whyuser");    
-    Axios.post("https://whyquestionnaire.herokuapp.com/whyquestion", { userid: userid })
-      .then((response) => {
-        if (response.data === false) {
-          history.push({ pathname: "/welcome" });
-        } else if (response.data.whyresult) {
-          let resultPath = "/whyresult/" + response.data.whyresult;
-          history.push({ pathname: resultPath });
-        } else {
-          setQindex(response.data[2]);
-          setQtext(response.data[0].question);
-          setOptions([...response.data[1]]);
-        }
-        if (qIndex === 1) {
-          backButton.current.style.display = "none";
-        } else {
-          backButton.current.style.display = "inline-block";
-        }
+    useEffect(() => {
+      //get the userid from the cookie stored and send it to /whyquestion api for getting the appropriate question for this user
+      let userid = Cookies.get("whyuser");
+      Axios.post("https://whyquestionnaire.herokuapp.com/whyquestion", {
+        userid: userid,
       })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [qIndex]);
-
-  const nextQ = () => {
-  let userid=Cookies.get("whyuser");    
-
-    document.getElementById("qform").reset();
-    if (choosenOption.length == 0) {
-      alert("Choose an option");
-    } else {
-      Axios.post(
-        "https://whyquestionnaire.herokuapp.com/saveanswer",
-        {
-          lockedanswer: choosenOption,
-          userid:userid
-        }
-      )
         .then((response) => {
-          setQindex("");
-          chooseOption("");
-          if (response.data.saved) {
+          // if a cookie doesn't exist redirect the user to welcome page where he can give his name
+          if (response.data === false) {
+            history.push({ pathname: "/welcome" });
+             // if a result is received, send user to result page
+          } else if (response.data.whyresult) {
+            let resultPath = "/whyresult/" + response.data.whyresult;
+            history.push({ pathname: resultPath });
+          } else {
+            // if question is receieved, save the data in appropriate useStates
+            setQindex(response.data[2]);
+            setQtext(response.data[0].question);
+            setOptions([...response.data[1]]);
+          }
+          //hide back button if the question asked is the first question
+          if (qIndex === 1) {
+            backButton.current.style.display = "none";
+          } else {
+            backButton.current.style.display = "inline-block";
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, [qIndex]);
+
+
+    const nextQ = () => {
+      //for saving the given option get the userid from cookie
+      let userid = Cookies.get("whyuser");
+      document.getElementById("qform").reset();
+      //check if option is choosen
+      if (choosenOption.length == 0) {
+        alert("Choose an option");
+      } else {
+        //send the userid and the option selected to /saveanswer api where the option will be saved
+        Axios.post("https://whyquestionnaire.herokuapp.com/saveanswer", {
+          lockedanswer: choosenOption,
+          userid: userid,
+        })
+          .then((response) => {
+            //when response receieved is true set the useState Qindex to empty so that the components can refresh with the useEffect 
+            //and redirect the user to /whyquestionnaire so that he sees the next question
+            if (response.data.saved) {
+                setQindex("");
+                chooseOption("");
+                history.push({ pathname: "/whyquestionnaire" });
+              }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    };
+
+    const goBack = () => {
+      //if the user wants to go back to the last question get the userid from cookie and send to /deletelastanswer api
+      let userid = Cookies.get("whyuser");
+      document.getElementById("qform").reset();
+      Axios.post("https://whyquestionnaire.herokuapp.com/deletelastanswer", {
+        userid: userid,
+      })
+        .then((response) => {
+          if (response.data.deleted) {
+            //when the last answer is deleted set the useStates to empty so that components  refresh with the useEffect 
+            //and redirect the user to /whyquestionnaire so that he sees the last question
+            setQindex("");
+            chooseOption("");
             history.push({ pathname: "/whyquestionnaire" });
           }
         })
         .catch((error) => {
           console.log(error);
         });
-    }
-  };
-
-  const goBack = () => {
-    document.getElementById("qform").reset();
-    let userid=Cookies.get("whyuser");    
-
-    Axios.post("https://whyquestionnaire.herokuapp.com/deletelastanswer",  { userid: userid })
-      .then((response) => {
-        if (response.data.deleted) {
-          setQindex("");
-          chooseOption("");
-          history.push({ pathname: "/whyquestionnaire" });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
- 
-if(!qIndex)
-  return (
-  <>
-<h1>Loading.....</h1>
-  </>);
-  else
-  return (
-    <>
-      <div className="row outblock">
-        <div className="quesbox col-xl-4 col-lg-4 col-md-8 col-sm-12 col-12">
-        <br />
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-            id="qform"
-            noValidate
-          >
-            <h3 id="qtext">
-              <span id="qtext" >Q{qIndex} / </span>
-              {qtext}
-            </h3>
-            <br />
-            {option.map((val, idx) => {
-              return (
-                <>
-                  <span className="radiobtn">{idx + 1}&nbsp;&nbsp;</span>
-                  <input
-                    type="radio"
-                    required
-                    value={val.id}
-                    name="option"
-                    className="radiobtn"
-                    onClick={(e) => {
-                      chooseOption(e.target.value);
-                    }}
-                  />
-                  <label>&nbsp;{val.option}</label>
-                  <br />
-                </>
-              );
-            })}
-            <br />
-            <br />
-           
-              <button
-                id="backbtn"
-                ref={backButton}
-                className="btn  btn-warning me-3"
-                onClick={goBack}
+    };
+//if qIndex has not been set, return a message which displays Loading.....
+    if (!qIndex)
+      return (
+        <>
+          <h1>Loading.....</h1>
+        </>
+      );
+      //if qIndex has been set, return the question and options
+    else
+      return (
+        <>
+          <div className="row outblock">
+            <div className="quesbox col-xl-4 col-lg-4 col-md-8 col-sm-12 col-12">
+              <br />
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+                id="qform"
+                noValidate
               >
-                Go Back
-              </button>
+                <h3 id="qtext">
+                  <span id="qtext">Q{qIndex} / </span>
+                  {qtext}
+                </h3>
+                <br />
+                {option.map((val, idx) => {
+                  return (
+                    <>
+                      <span className="radiobtn">{idx + 1}&nbsp;&nbsp;</span>
+                      <input
+                        type="radio"
+                        required
+                        value={val.id}
+                        name="option"
+                        className="radiobtn"
+                        onClick={(e) => {
+                          chooseOption(e.target.value);
+                        }}
+                      />
+                      <label>&nbsp;{val.option}</label>
+                      <br />
+                    </>
+                  );
+                })}
+                <br />
+                <br />
+               
+                <button
+                  id="backbtn"
+                  ref={backButton}
+                  className="btn  btn-warning me-3"
+                  onClick={goBack}
+                >
+                  Go Back
+                </button>
 
-              <button className="btn  btn-success" onClick={nextQ}>
-                Save and Next
-              </button>
-            </form>
-          
-        </div>
-      </div>
-    </>
-  );
- 
-}
+                <button className="btn  btn-success" onClick={nextQ}>
+                  Save and Next
+                </button>
+              </form>
+            </div>
+          </div>
+        </>
+      );
+  }
 
-export default App;
+  export default App;
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,29 +1,38 @@
-import { useParams, useHistory } from "react-router-dom";
-import React, { useState, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+import React, { useState} from "react";
 import Cookies from "js-cookie";
-
 import Axios from "axios";
 
 const Welcome = () => {
   const history = useHistory();
-
+  //Check if a cookie is already created.  If it is return to whyquestionnaire
   if (Cookies.get("whyuser")) {
     history.push({ pathname: "/whyquestionnaire" });
   }
+  //Store the name entered by user in the useState
   const [name, setName] = useState(null);
+
+
   const addUser = () => {
+    //Check if the user doesn't enter name.
     if (!name) {
       alert("Please enter your name first");
     } else {
-      Cookies.set("whyusername", name);
+      //If the user sets the name, create a cookie with his name
+      Cookies.set("whyusername", name,{expires:1/24});
+
+      //send a request to api to  add this user into the database
       Axios.post(
         `https://whyquestionnaire.herokuapp.com/adduser?username=${name}`,
       )
         .then((response) => {
+
           if (response.data.userid) {
-            Cookies.set("whyuser", response.data.userid);
+            //store the userid received in a cookie.
+            Cookies.set("whyuser", response.data.userid,{expires:1/24});
             history.push({ pathname: "/whyquestionnaire" });
-          } else history.push({ pathname: "/welcome" });
+          } else
+           history.push({ pathname: "/welcome" });
         })
         .catch((error) => {
           console.log(error);
